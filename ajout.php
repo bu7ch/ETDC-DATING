0dd6abd9385c80af886148625223dcf03f4a9761
Rodolphe <dutchr@live.fr>
2021-08-24 12:00:08 +0200

    :sparkles: ajout.php DONE!

--- ajout.php
+++ ajout.php
@@ -1,4 +1,7 @@
 <?php 
+require_once('initialize.php')
+?>
+<?php 
 include_once('./shared/header.php');
 ?>
 
@@ -18,15 +21,16 @@ include_once('./shared/header.php');
         </tr>
         <tr>
           <td>Département:</td>
-          <td><input type="text" name="depart" size="30"></td>
+          <td><input type="text" name="departement" size="30"></td>
         </tr>
         <tr>
           <td>Mail:</td>
-          <td><input type="text" name="mail" size="30"></td>
+          <td><input type="mail" name="email" size="30"></td>
         </tr>
       </tbody>
     </table>
   </fieldset>
+
   <!-- 2ieme groupe -->
   <fieldset>
   <legend>Vos pratiques sportives</legend>
@@ -36,17 +40,73 @@ include_once('./shared/header.php');
       <tr>
         <td>Sport pratiqué:</td>
         <td>
-          <select name="design">
+          <select name="designation">
             <option value="NULL">Choisissez le sport !</option>;
             <?php 
             // creation dynamique de la liste de séléction
             //connexion 
             // lire la table sport
+            $requete = "SELECT id_sport , designation FROM sport ORDER BY designation";
+            $result = $connexion->query($requete);
+            if($result){
+              while($row = $result->fetch()) {
+                echo '<option value='.$row[0].'>'.$row[1].'</option>';
+              }
+            }
             ?>
+          </select>
+          OU : ajouter un sport à la liste <input type="text" name="nomsport" size="30" />
+          <input type="submit" name="ajout" value="ajouter" />
+        </td>
+      </tr>
+      <tr>
+        <td>Niveau:</td>
+        <td>
+          <select name="niveau" >
+            <option value="1">Débutant</option>
+            <option value="2">Confirmé</option>
+            <option value="3">Pro</option>
+            <option value="4">Supporter</option>
           </select>
         </td>
       </tr>
+      <td>
+        <input type="submit" name="envoie" value="Envoyer" />
+      </td>
+      <td>
+        <input type="reset" name="efface" value="Effacer" />
+      </td>
     </tbody>
   </table>
   </fieldset>
 </form>
+
+<?php 
+// insert dans personne
+if (isset($_POST['envoie'])) {
+  $req_pers = "INSERT INTO personne(id_personne, nom, prenom, departement, email) VALUES
+  (NULL,'".$_POST['nom']."','".$_POST['prenom']."','".$_POST['departement']."','".$_POST['email']."' )";
+  if($connexion->query($req_pers)) {
+    $id_personne=$connexion->lastInsertId();
+  }
+  // insertion dans la table pratique
+
+  $id_sport = $_POST['designation'];
+  $niveau = $_POST['niveau'];
+
+  $req_pratique = "INSERT INTO pratique (id_personne,id_sport,niveau) VALUES ('$id_personne', '$id_sport', '$niveau')";
+  if ($connexion->query($req_pratique)) {
+    echo "<div> DONNEES INSEREES dans la table pratique </div>";
+  }
+}
+
+// Ajouter sport 
+
+if(isset($_POST['ajout'])){
+  $req_sport = "INSERT INTO sport (id_sport, designation) VALUES (NULL, '".$_POST['nomsport']."')";
+  if($connexion->query($req_sport)){
+    echo "<div> DONNEES INSEREES dans la table sport </div>";
+  } 
+}
+
+?>
